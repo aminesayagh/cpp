d2b5e5d0020be802384c8cb40de769e3dc074ebe
aminesayagh <aminesayagh1997@gmail.com>
2022-12-21 07:55:51 +0100

    virtual function

---
+++ 5-pureVirtualFunction/VirtualFunction.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class Instrument {
+    public:
+        virtual void MakeSound() = 0;
+};
+
+class Accordion : public Instrument {
+    public:
+        void MakeSound() {
+            cout << "Accordion playing..." << endl;
+        }
+};
+
+class Piano : public Instrument {
+    public:
+        void MakeSound() {
+            cout << "Piano playing..." << endl;
+        }
+};
+
+int main () {
+    Instrument *instrumentAccordion = new Accordion();
+    instrumentAccordion->MakeSound();
+
+    Instrument *instrumentPiano = new Piano();
+    instrumentPiano->MakeSound();
+
+    Instrument *instrument[2] = { instrumentPiano, instrumentAccordion };
+    for(int i = 0; i < 2; i++){
+        instrument[i]->MakeSound();
+    }
+
+    return 0;
+
+}
