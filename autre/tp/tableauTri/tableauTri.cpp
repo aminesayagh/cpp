faac6ef6f116095eedfe44fe870ff7c8f36ed4d1
aminesayagh <aminesayagh1997@gmail.com>
2022-12-31 19:57:24 +0100

    i

--- autre/tp/tableauTri/tableauTri.cpp
+++ autre/tp/tableauTri/tableauTri.cpp
@@ -1,34 +1,42 @@
 #include "tableauTri.h"
 
 // CONSTRUCTORS
-TableauTri::TableauTri(int taille){
+TableauTri::TableauTri(int taille)
+{
     taille = taille;
     tab = new float[taille];
 }
 
-TableauTri::TableauTri(int taille): taille(taille){
+TableauTri::TableauTri(int taille) : taille(taille)
+{
     tab = new float[taille];
 }
 
-TableauTri::TableauTri(const TableauTri &t): taille(t.taille) {
+TableauTri::TableauTri(const TableauTri &t) : taille(t.taille)
+{
     delete tab;
     tab = new float[t.taille];
-    for(int i = 0; i < t.taille; i++){
+    for (int i = 0; i < t.taille; i++)
+    {
         tab[i] = t.tab[i];
     }
 }
 
-TableauTri::~TableauTri() {
+TableauTri::~TableauTri()
+{
     taille = 0;
     delete tab;
 }
 
-TableauTri& TableauTri::operator=(const TableauTri &t){
-    if(this != &t) {
+TableauTri &TableauTri::operator=(const TableauTri &t)
+{
+    if (this != &t)
+    {
         delete tab;
         taille = t.taille;
         tab = new float[t.taille];
-        for(int i=0; i<t.taille; i++) {
+        for (int i = 0; i < t.taille; i++)
+        {
             tab[i] = t.tab[i];
         }
     }
@@ -37,18 +45,24 @@ TableauTri& TableauTri::operator=(const TableauTri &t){
 }
 
 // METHODS
-int TableauTri::frequence(float x){
+int TableauTri::frequence(float x)
+{
     int n = 0;
-    for(int i= 0; i < taille; i++) {
-        if(tab[i] == x) n++;
+    for (int i = 0; i < taille; i++)
+    {
+        if (tab[i] == x)
+            n++;
     }
     return n;
 }
 
-void TableauTri::supprimer(float x) {
+void TableauTri::supprimer(float x)
+{
     int j = 0;
-    for(int i = 0; i < taille; i++){
-        if(tab[i] == x) {
+    for (int i = 0; i < taille; i++)
+    {
+        if (tab[i] == x)
+        {
             j++;
             taille--;
         }
@@ -56,19 +70,26 @@ void TableauTri::supprimer(float x) {
     }
 }
 
-bool TableauTri::operator>(float x) {
-    for(int i = 0; i < taille; i++) {
-        if(tab[i] == x) return i;
-        if(tab[i] > x) return false;
+bool TableauTri::operator>(float x)
+{
+    for (int i = 0; i < taille; i++)
+    {
+        if (tab[i] == x)
+            return i;
+        if (tab[i] > x)
+            return false;
     }
     return true;
 }
 
-TableauTri& TableauTri::operator+(float x) {
+TableauTri &TableauTri::operator+(float x)
+{
     int j = 0;
     float *newTab = new float[taille + 1];
-    for(int i = 0; i < taille; i++){
-        if(tab[i+1] >= x && j != 1) {
+    for (int i = 0; i < taille; i++)
+    {
+        if (tab[i + 1] >= x && j != 1)
+        {
             newTab[i] = x;
             j++;
             taille++;
@@ -81,14 +102,17 @@ TableauTri& TableauTri::operator+(float x) {
     return *this;
 }
 
-TableauTri& TableauTri::operator+(const TableauTri& t){
-    int j= 0;
+TableauTri &TableauTri::operator+(const TableauTri &t)
+{
+    int j = 0;
     float *newTab = new float(taille + t.taille);
 
-    for(int i = 0; i < taille; i++){
-        while(tab[i+1] >= t.tab[j]){
+    for (int i = 0; i < taille; i++)
+    {
+        while (tab[i + 1] >= t.tab[j])
+        {
             taille++;
-            newTab[i+j] = t.tab[j];
+            newTab[i + j] = t.tab[j];
             j++;
         }
         newTab[i + j] = tab[i];
@@ -99,33 +123,23 @@ TableauTri& TableauTri::operator+(const TableauTri& t){
     return *this;
 }
 
-TableauTri& TableauTri::operator--(){
-    for(int i = 0; i < taille; i++){
+TableauTri &TableauTri::operator--()
+{
+    for (int i = 0; i < taille; i++)
+    {
         tab[i]++;
     }
     return *this;
 }
 
-
-TableauTri& TableauTri::operator*(float r){
-    for(int i = 0; i < taille; i++){
+TableauTri &TableauTri::operator*(float r)
+{
+    for (int i = 0; i < taille; i++)
+    {
         tab[i] *= r;
     }
     return *this;
 }
 
-ostream& operator<<(ostream& ot, const TableauTri &t){
-    for(int i = 0; i < t.taille; i++){
-        ot << "tab[" << i << "] = " << t.tab[i] << endl;
-    }
-    return ot;
-}
-
-istream& operator>>(istream& in, TableauTri &t) {
-    for(int i = 0; i < t.taille; i++){
-        cout << "Saisir tab[" << i << "] = "; 
-        in >> t.tab[i];
-    }
-    return in;
-}
-
+//  TableauTriResp
+TableauTriResp::TableauTriResp(int taille) : TableauTri(taille) {}
--- autre/tp/tableauTri/tableauTri.h
+++ autre/tp/tableauTri/tableauTri.h
@@ -2,71 +2,91 @@
 using namespace std;
 using std::string;
 
-class TableauTri{
-    protected:
-        static const int tailleDefault = 50;
-        float *tab;
-        int taille;
-        int sommet;
-    public:
+class TableauTri
+{
+protected:
+    static const int tailleDefault = 50;
+    float *tab;
+    int taille;
+    int sommet;
 
-        TableauTri(int taille = tailleDefault);
-        TableauTri(int taille);
-        TableauTri(const TableauTri &t);
-        ~TableauTri();
+public:
+    TableauTri(int taille = tailleDefault);
+    TableauTri(int taille);
+    TableauTri(const TableauTri &t);
+    ~TableauTri();
 
-        TableauTri& operator=(const TableauTri &t);
-        bool operator>(float x);
-        TableauTri& operator+(float x);
-        TableauTri& operator+(const TableauTri &t);
-        TableauTri &operator--();
-        TableauTri &operator*(float r);
+    TableauTri &operator=(const TableauTri &t);
+    bool operator>(float x);
+    TableauTri &operator+(float x);
+    TableauTri &operator+(const TableauTri &t);
+    TableauTri &operator--();
+    TableauTri &operator*(float r);
 
-        friend ostream& operator<<(ostream&, const TableauTri &);
-        friend istream& operator>>(istream&, TableauTri &);
-
-        int frequence(float x);
-        void supprimer(float x);
-
-        int getTaille(){
-            return taille;
+    friend ostream &operator<<(ostream &os, const TableauTri &t)
+    {
+        for (int i = 0; i < t.taille; i++)
+        {
+            os << "tab[" << i << "] = " << t.tab[i] << endl;
         }
-        float* getTab(){
-            return tab;
+        return os;
+    }
+    friend istream &operator>>(istream &is, TableauTri &t)
+    {
+        for (int i = 0; i < t.taille; i++)
+        {
+            cout << "Saisir tab[" << i << "] = ";
+            is >> t.tab[i];
         }
+        return is;
+    }
+
+    int frequence(float x);
+    void supprimer(float x);
+
+    int getTaille()
+    {
+        return taille;
+    }
+    float *getTab()
+    {
+        return tab;
+    }
 };
 
-class TableauTriResp:TableauTri{
-    private:
-        int elementRepeter;
-        int elementNonRepeter;
-        bool setCaseOfTable(float newValue){
-            bool noError = (*this)> newValue;
-            if(!noError) {
-                cout << "ERROR" << endl;
-                return false;
+class TableauTriResp : TableauTri
+{
+private:
+    int elementRepeter = 0;
+    int elementNonRepeter = 0;
+
+public:
+    TableauTriResp(int taille);
+    TableauTriResp(const TableauTri &t) : TableauTri(t) {}
+
+    friend ostream &operator<<(ostream &os, const TableauTriResp &t)
+    {
+        os << t << endl;
+        return os;
+    }
+    friend istream &operator>>(istream &is, TableauTriResp &t)
+    {
+        for (int i = 0; i < t.taille; i++)
+        {
+            cout << "Saisir tab[" << i << "] = ";
+            is >> t.tab[i];
+
+            int nombreFrequence = t.frequence(t.tab[i]);
+            if (nombreFrequence == 0)
+            {
+                t.elementNonRepeter++;
             }
-
-            int nombreFrequence = this->frequence(newValue);
-            if(nombreFrequence==0) {
-                elementNonRepeter++;
-            }else if(nombreFrequence == 1){
-                elementNonRepeter--;
-                elementRepeter++;
+            else if (nombreFrequence == 1)
+            {
+                t.elementNonRepeter--;
+                t.elementRepeter++;
             }
-            return true;
         }
-    public:
-        TableauTriResp(int taille): TableauTri(taille) {
-
-        }
-        TableauTriResp(const TableauTri &t): TableauTri(t) {}
-
-        friend ostream& operator<<(ostream& os, const TableauTriResp &t){
-            os << t << endl;
-        }
-        friend istream& operator>>(istream&, TableauTriResp &t){
-            
-        }
-        
+        return is;
+    }
 };
