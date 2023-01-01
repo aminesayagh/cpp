ee8b7cb59de9b816e3ecbdabac44aa178ddfbbca
aminesayagh <aminesayagh1997@gmail.com>
2023-01-01 21:50:17 +0100

    impoter

--- autre/tp/tableauTri/test.cpp
+++ autre/tp/tableauTri/test.cpp
@@ -25,10 +25,10 @@ int main()
 
     TableauTri monTableau4 = monTableau1;
 
-    float value;
-    cout << "Value : " << endl;
-    cin >> value;
-    int frequenceTableau1 = monTableau1.frequence(value);
+    float valueFrequence;
+    cout << "Value frequence : " << endl;
+    cin >> valueFrequence;
+    int frequenceTableau1 = monTableau1.frequence(valueFrequence);
 
     if(frequenceTableau1 == -1){
         cout << "frequence not exist" << endl;
@@ -36,7 +36,14 @@ int main()
         cout << "frequence exist" << endl;
     }
 
+    float valueSuppimer;
+    cout << "Value a supprimer : " << endl;
+    cin >> valueSuppimer;
     
+    monTableau1.supprimer(valueSuppimer);
+
+    
+
 
     return 0;
 }
